131597359c94e5a6d0cf403c0267b9d3f61bafa4
piyush.chourasiya@delhivery.com <piyush.chourasiya@delhivery.com>
2017-08-20 22:42:01 +0530

    .

--- deploy_api/settings.py
+++ deploy_api/settings.py
@@ -117,4 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = '/static_root/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'static'),
+)
